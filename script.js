fd22396b89f31680f947a26ea9fc777aae254a1d
OneGiantTree <115727986+TheOneGiantTree@users.noreply.github.com>
2023-08-21 18:29:30 -0400

    add random function, clean up code

--- script.js
+++ script.js
@@ -3,13 +3,29 @@ const main = document.getElementById('main');
 const header = document.getElementById('header');
 
 let userInput = 16;
+let randomMode = false;
 
 let resetButton = document.createElement('button');
 resetButton.textContent = 'Reset?';
 header.appendChild(resetButton);
 resetButton.addEventListener('click', resetPrompt);
 
+let randomButton = document.createElement('button');
+randomButton.textContent = 'Randomize';
+header.appendChild(randomButton);
+randomButton.addEventListener('click', toggleRandom);
+
 setGrid(userInput);
+
+function toggleRandom(){
+    randomMode = !randomMode;
+    if(randomMode){
+        randomButton.classList.add('on');
+    }
+    else {
+        randomButton.classList.remove('on');
+    }
+}
 
 function resetPrompt(){
     userInput = prompt('How big would you like the grid? (1-64)');
@@ -33,8 +49,18 @@ function setGrid(input){
 
 function hover(cell){
     cell.addEventListener('mouseenter', () => {
-        changeColor(cell);
+        if(randomMode){
+            changeRandom(cell);
+        }
+        else{
+            changeColor(cell);
+        }
     })
+}
+
+function changeRandom(cell){
+    const randomColor = '#' + Math.random().toString(16).slice(-6);
+    cell.style.backgroundColor = randomColor;
 }
 function changeColor(cell){
     cell.classList.add('black');
